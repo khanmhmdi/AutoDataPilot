09848699cff9f166acd48827d429a7f4c94df022
khanmhmdi <khanmohamady1381@gmail.com>
2023-03-10 21:23:59 +0330

    make code oop!

--- src.py
+++ src.py
@@ -1,24 +1,12 @@
-import pandas as pd
-
-# Define chunk size (number of rows per chunk)
-chunk_size = 100000
-
-# Initialize an empty dataframe to hold the results
-df = pd.DataFrame()
-
-# Open the file in chunks and process each chunk
-for chunk in pd.read_csv('filename.csv', chunksize=chunk_size):
-    # Process the chunk
-    # For example, you could filter the data or perform some computation on it
-    processed_chunk = chunk.loc[chunk['column_name'] == 'some_value']
+class Data:
+    def __init__(self, PATH, chunk_size=10000 , RAM_SIZE = 4):
+        self.PATH = PATH
+        self.chunk_size = chunk_size
+        self.RAM_SIZE = 4
     
-    # Append the processed chunk to the results dataframe
-    df = pd.concat([df, processed_chunk])
-
-# Clean up the results dataframe (optional)
-# For example, you could remove duplicates or null values
-df = df.drop_duplicates().dropna()
-
-# Output the results
-print(df.head())
-
+    def read_data(self):
+        df = pd.DataFrame()
+        for chunk in pd.read_csv(self.PATH, chunksize=self.chunk_size):
+            #TO_DO
+            df = pd.concat([df, chunk])
+        print(df.head())
